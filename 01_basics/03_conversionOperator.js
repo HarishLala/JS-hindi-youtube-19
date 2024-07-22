1fcdba32b2b5d04a5fdd19f1f8b8318a3af974d3
HarishLala <harishkumarlala2409@gmail.com>
2024-07-22 14:26:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,22 @@
+// let score= "77dhoni"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc" =>NaN
+//  true=>1; false=>0
+// let isLoggedIn= ""
+// let booleanIsLoggnedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggnedIn)
+// 1=>true ;0=>false
+//"" => false
+//"harish"=>true
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
