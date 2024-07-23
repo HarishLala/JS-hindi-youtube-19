bd74f0cacdfee477c2a0dd0bb97ebf015a632efd
HarishLala <harishkumarlala2409@gmail.com>
2024-07-23 15:22:13 +0000

    consversionOperator

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -18,5 +18,59 @@
 //"harish"=>true
 let someNumber =33
 let stringNumber =String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+// *****************************Operations*******************************
+let value=3
+let negValue=-value
+// console.log(negValue) 
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%2)
+
+// let str1 ="hello"
+// let str2 =" harish"
+
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2 +2);
+// console.log(1+2 +"2")
+
+// console.log(-true);
+// console.log(+true);
+// console.log(-false);
+// console.log(+false);
+// console.log(+false);
+
+
+// let num1,num2,num3
+// num1=num2=num3 = 3+3
+// console.table(num1);
+
+// let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+
+
+// let x = 3;
+// const y=x++;
+// console.log(`x:${x}  , y:${y}`);
+// let a = 3;
+// const b =++a;
+// console.log(`a:${a}  , b:${b}`);
+let i=0;
+// let a = ++ i;
+// console.log('a' ,a);
+// console.log('i' ,i);
+let a =  i++;
+console.log('a' ,a);
+console.log('i' ,i);
+// let i=0;
+// console.log(++i ==i++)
