1c2e700220f790eefea150430066c5cbed3820df
HarishLala <harishkumarlala2409@gmail.com>
2024-07-28 17:11:48 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,11 +4,26 @@ const repoCount=50
 //console.log(`Hello My Name Is ${name} and My repo count is ${repoCount}`);
 
  
-const gameName =new String('harish')
+const gameName =new String('Harish-Kumar-Lala')
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
 // console.log(gameName.charAt(2));
+//  console.log(gameName.indexOf('k'));
 
-console.log(gameName.indexOf(2));
+// const newString= gameName.substring(0 ,4);
+// console.log(newString);
+// const anotherString =gameName.slice(-4,2)
+// console.log(anotherString);
+
+// const newStringOne ="   harish   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url="https://harish.com/harish%20kumarlala"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('johi'));
+console.log(gameName.split('-'))
