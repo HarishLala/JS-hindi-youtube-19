cee677955cd52b5004f5217e0e191c60f620d57c
HarishLala <harishkumarlala2409@gmail.com>
2024-07-29 12:10:11 +0000

    nums_and_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -24,10 +24,11 @@
 // console.log(Math.max(6.8,9,7,5,6,2,1));
 
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min=10
 const max=20;
 
+console.log(Math.floor(Math.random ()* (max - min +1)) + min);
