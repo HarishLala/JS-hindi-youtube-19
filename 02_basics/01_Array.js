a98f77f24fb974749ae374250dcc674cc128747c
HarishLala <harishkumarlala2409@gmail.com>
2024-07-30 17:17:03 +0000

    Array

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -1,4 +1,36 @@
 // Array
+// const myArr =[0,1,2,3,4,5,6,true ,'harish']
+const myArr =[0,1,2,3,4,5,6]
 
-const myArr =[0,1,2,3,4,5,6,true ,'harish']
-console.log(myArr);
+// console.log(myArr[3]);
+// const myHeros=["saktiman","naagraj"]
+
+// const myArr2 = new Array(1,2,3,4,5,6)
+// console.log(myHeros);
+// console.log(myArr2);
+
+//Array Method
+// myArr.push(69);
+// myArr.push(99);
+//myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(6));
+
+// const newArr= myArr.join()
+// console.log( typeof newArr);
+
+
+//slice,splice
+console.log("A",myArr);
+const myn1 =myArr.slice(1,3)
+console.log(myn1);
+console.log("B" ,myArr);
+
+const myn2 =myArr.splice(1,3)
+console.log("C" ,myArr);
+console.log(myn2);
