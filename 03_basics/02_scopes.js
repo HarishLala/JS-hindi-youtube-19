431c166d3bde71b9b0efb427f56bd18ba0366876
HarishLala <harishkumarlala2409@gmail.com>
2024-08-06 04:50:36 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+// var c=300
+// {} scope
+let a=200
+if(true)
+{
+    let a=10
+    const b=20
+     c=30
+     console.log("Inner :" ,a);
+     
+
+}
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    }
+console.log(a);
+// console.log(b);
+// console.log(c);
