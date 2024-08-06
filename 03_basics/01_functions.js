399fe138bebe631888277dd62e483d8d374134ac
HarishLala <harishkumarlala2409@gmail.com>
2024-08-06 04:41:16 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,5 +40,31 @@ function loginUserName(username="som") {
     return `${username} Just Logged In` 
 }
 // console.log(loginUserName("Harish"));
-console.log(loginUserName());
+// console.log(loginUserName());
 
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,600));
+
+const user={
+    username :"hitesh",
+    price : 199
+}
+function handleObject(anyObject) {
+    
+    console.log(`username is ${anyObject.username} is price ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:499
+})
+
+const myNewArray =[200,400,500,600,700]
+function returnSecondvalue(getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondvalue(myNewArray));
+console.log(returnSecondvalue([200,400,500,600,700]));
