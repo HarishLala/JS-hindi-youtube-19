49b7efc292861af1c66fec35ace34e73b2328fee
HarishLala <harishkumarlala2409@gmail.com>
2024-08-12 17:21:25 +0000

    truthly

---
+++ 04_Control_flow/truthly.js
@@ -0,0 +1,36 @@
+ const userEmail = []//"harish@gmail.com"
+if(userEmail)
+{
+    console.log("Got user Email");
+}else{
+    console.log("Don't Have User Email")
+}
+
+// if(userEmail.length ===0){
+//     console.log("Array is empty");
+    
+//}
+
+const emptyObj ={}
+if(Object.keys(emptyObj).length===0){
+    console.log("Object Is empty");
+    
+}
+//Nullish Coalescing Operator(??): null Undefined 
+
+let val1;
+// val1 =25 ?? 30
+//val1 =null ?? 21
+// val1 =undefined ?? 15
+
+val1 =null ?? 10 ?? 20
+console.log(val1);
+
+
+//Terniary Operator
+const iceTeaPrice =100
+iceTeaPrice <=80 ? console.log("less than 80"): 
+console.log("more than 80");
+;
+
+
