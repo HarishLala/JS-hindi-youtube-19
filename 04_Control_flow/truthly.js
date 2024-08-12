03c703c260a7370e144b8331f9597a1347a31ad5
HarishLala <harishkumarlala2409@gmail.com>
2024-08-12 17:31:29 +0000

    true

--- 04_Control_flow/truthly.js
+++ 04_Control_flow/truthly.js
@@ -5,6 +5,12 @@ if(userEmail)
 }else{
     console.log("Don't Have User Email")
 }
+//falsy values
+//false,0,-0,bigInt 0n,"", null,undefined,NaN
+
+//truthy Values
+//"0",'false',"",[],{},function(){}
+
 
 // if(userEmail.length ===0){
 //     console.log("Array is empty");
