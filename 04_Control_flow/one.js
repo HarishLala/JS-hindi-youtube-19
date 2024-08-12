f83079f8f3e2cb40ba25a0485592f17e64ad811f
HarishLala <harishkumarlala2409@gmail.com>
2024-08-12 17:32:33 +0000

    one

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -49,6 +49,6 @@ if(userLoggedIn && debitCard && 2==2)
     console.log("Allow to buy Course");
     
 }
-if(loggedInFromGoogle || loggedInFromEmail||){
+if(loggedInFromGoogle || loggedInFromEmail){
     console.log("User Logged in");    
 }
