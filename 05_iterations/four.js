14560452792a2da2bc2407138a2af30939ccfc10
HarishLala <harishkumarlala2409@gmail.com>
2024-08-16 17:55:49 +0000

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject={
+    js:'JavaScript',
+    cpp:'C++',
+    rb:'ruby',
+    swift :'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} Shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming =['js','java','c++','c','swift','rubby']
+
+for (const key in programming) {
+  console.log(programming[key]);
+  
+}
+// const map= new Map()
+// map.set('IN',"INDIA")
+// map.set('USA',"United States of America")
+// map.set('Fr',"France")
+
+// for (const key in map) {
+//     console.log(map[key]);
+    
+// }
