dd8bc1355961de66da89403e63a372a930347e31
HarishLala <harishkumarlala2409@gmail.com>
2024-10-28 18:05:19 +0000

    oops

---
+++ 10_classes_and_loop/oop.js
@@ -0,0 +1,37 @@
+const user={
+    username:'sonali',
+    loginCount:8,
+    signedIn:true,
+
+    getUserDetails:function () {
+        // console.log("Got User deatils from database");
+        // console.log(`Username:${this.username}`);
+        console.log(this);
+        
+    }
+}
+
+
+// console.log(user.username);
+//not a reference  it is a Method 
+// console.log(user.getUserDetails);
+// console.log(user.getUserDetails());
+//  console.log(this);
+ 
+//Constructor Functions
+//New One instance multiple instance create 
+// const promisesOne = new  Promise()
+// const date = new Date()
+
+function User(username,loginCount,isLoggedIn) {
+    this.username=username;
+    this.loginCount=loginCount;
+    this.isLoggedIn=isLoggedIn;
+    //Implicity it working 
+    return this
+}
+const userOne =new User('Sonali', 12, true)
+const userTwo =new  User('Harish', 11, false)
+//This one created overridethe method
+console.log(userOne);
+console.log(userTwo);
